5c9d096552806e28e3c245fd78c71e7657515c8f
Jack Franco Llaique Chullunquia <jllaiquec@unsa.edu.pe>
2025-06-25 01:01:15 -0500

    🔧 Configuración final para deploy en Vercel 2

--- vercel_build.sh
+++ vercel_build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Activar entorno virtual (Vercel ya lo hace)
-# Instalar dependencias
 pip install -r requirements.txt
-
-# Migraciones (opcional pero recomendado si tienes modelos)
-python manage.py migrate
-
-# Recolectar archivos estáticos (si usas WhiteNoise o CSS/JS)
 python manage.py collectstatic --noinput
+python manage.py migrate
