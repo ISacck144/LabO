68b0bb4fb9d6ace6d3d744f7ea4914cca57d86e1
Jack Franco Llaique Chullunquia <jllaiquec@unsa.edu.pe>
2025-06-25 12:07:22 -0500

    Cmabios en json

--- index.py
+++ index.py
@@ -2,4 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "MyDjangoProject.settings")
-app = get_wsgi_application()  # ✅ Nota: 'app', no 'application' ni 'handler'
+application = get_wsgi_application()  # ✅ Nota: 'app', no 'application' ni 'handler'
